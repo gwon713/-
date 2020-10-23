92530573f22e5aca2f51425b92184e8fac2ef2ab
gwon713 <62048410+gwon713@users.noreply.github.com>
2020-10-23 19:34:20 +0900

    connect MongoDB basic Restful

---
+++ survey/src/main/java/com/example/survey/User.java
@@ -0,0 +1,53 @@
+package com.example.survey;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
